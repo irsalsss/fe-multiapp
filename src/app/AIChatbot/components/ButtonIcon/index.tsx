6b03199401bca6981209ab1a837abc54a939d28d
Muhamad Irsal <muhamadirsal@gmail.com>
2025-05-12 08:44:34 +0700

    feature: add button icon

---
+++ src/app/AIChatbot/components/ButtonIcon/index.tsx
@@ -0,0 +1,48 @@
+import { useMemo } from 'react';
+import { twMerge } from 'tailwind-merge';
+import { ButtonSize, ButtonType } from './types';
+
+interface ButtonIconProps {
+  type: ButtonType;
+  size: ButtonSize;
+  icon: React.ReactNode;
+  onClick?: () => void;
+}
+
+const ButtonIcon = ({ type, size, icon, onClick }: ButtonIconProps) => {
+  const buttonSize = useMemo(() => {
+    if (size === ButtonSize.Small) {
+      return 'w-6 h-6';
+    }
+
+    if (size === ButtonSize.Medium) {
+      return 'w-8 h-8';
+    }
+
+    return 'w-10 h-10';
+  }, [size]);
+
+  const buttonType = useMemo(() => {
+    if (type === ButtonType.Primary) {
+      return 'bg-gray-700';
+    }
+
+    return 'bg-green-100';
+  }, [type]);
+
+  return (
+    <button
+      type="button"
+      className={twMerge(
+        'cursor-pointer rounded-lg flex items-center justify-center',
+        buttonSize,
+        buttonType
+      )}
+      onClick={onClick}
+    >
+      {icon}
+    </button>
+  );
+};
+
+export default ButtonIcon;
---
+++ src/app/AIChatbot/components/ButtonIcon/types.ts
@@ -0,0 +1,10 @@
+export enum ButtonType {
+  Primary = 'primary',
+  Secondary = 'secondary',
+}
+
+export enum ButtonSize {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+}
